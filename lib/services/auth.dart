7c1d991dfd2214b466d185b6a281666054a6e1bd
Moti Bartov <motib@tikalk.com>
2019-04-01 21:40:12 +0300

    Sign in with Google Update base auth class

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -1,47 +1,51 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:meta/meta.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
-class User{
+class User {
   final String uid;
+
   User({@required this.uid});
 }
 
-
-abstract class BaseAuth{
+abstract class BaseAuth {
   Stream<User> get onAuthStateChanged;
+
   Future<User> currentUser();
+
   Future<User> singInAnonymously();
+
   Future<void> signOut();
+
+  Future<User> signInWithGoogle();
 }
 
-class Auth implements BaseAuth{
-
+class Auth implements BaseAuth {
   final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
 
-  User _userFromFirebase(FirebaseUser firebaseUser){
-    if(firebaseUser == null){
+  User _userFromFirebase(FirebaseUser firebaseUser) {
+    if (firebaseUser == null) {
       return null;
     }
     return User(uid: firebaseUser.uid);
   }
 
   @override
-  Stream<User> get onAuthStateChanged{
-    return _firebaseAuth.onAuthStateChanged.map((firebaseUser){
+  Stream<User> get onAuthStateChanged {
+    return _firebaseAuth.onAuthStateChanged.map((firebaseUser) {
       return _userFromFirebase(firebaseUser);
     });
   }
 
-
   @override
   Future<User> currentUser() async {
-    FirebaseUser user =  await _firebaseAuth.currentUser();
+    FirebaseUser user = await _firebaseAuth.currentUser();
     return _userFromFirebase(user);
   }
 
   @override
   Future<User> singInAnonymously() async {
-    FirebaseUser user =  await _firebaseAuth.signInAnonymously();
+    FirebaseUser user = await _firebaseAuth.signInAnonymously();
     return _userFromFirebase(user);
   }
 
@@ -49,4 +53,24 @@ class Auth implements BaseAuth{
   Future<void> signOut() async {
     return await _firebaseAuth.signOut();
   }
+
+  @override
+  Future<User> signInWithGoogle() async {
+    GoogleSignIn googleSignIn = GoogleSignIn();
+    GoogleSignInAccount account = await googleSignIn.signIn();
+    if (account != null) {
+      GoogleSignInAuthentication authentication = await account.authentication;
+      if(authentication.idToken != null && authentication.accessToken != null){
+        FirebaseUser user = await _firebaseAuth.signInWithGoogle(
+          idToken: authentication.idToken,
+          accessToken: authentication.accessToken,
+        );
+        return _userFromFirebase(user);
+      }
+      throw StateError("Google sign in failed");
+    } else {
+      throw StateError("Google sign in aborted");
+    }
+  }
+
 }
