d5c3cc02cb2612ea182cf7538edd4fd354faf445
Moti Bartov <motib@tikalk.com>
2019-04-01 22:34:20 +0300

    SignOut, Facebook settings

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -51,6 +51,8 @@ class Auth implements BaseAuth {
 
   @override
   Future<void> signOut() async {
+    final googleSignIn = GoogleSignIn();
+    await googleSignIn.signOut();
     return await _firebaseAuth.signOut();
   }
 
