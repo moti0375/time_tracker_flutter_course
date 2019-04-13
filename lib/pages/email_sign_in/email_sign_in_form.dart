60074b9dc7363c2869d007c51a3818ec7e8f6d01
Moti Bartov <motib@tikalk.com>
2019-04-13 10:18:08 +0300

    StatefulWidget form

--- lib/pages/email_sign_in/email_sign_in_form.dart
+++ lib/pages/email_sign_in/email_sign_in_form.dart
@@ -1,26 +1,34 @@
 import 'package:flutter/material.dart';
 import 'package:time_tracker_flutter_course/common_widgets/form_submit_button.dart';
 
-class EmailSignInForm extends StatelessWidget {
 
-  String email;
+
+class EmailSignInForm extends StatefulWidget {
+
+  @override
+  _EmailSignInFormState createState() => _EmailSignInFormState();
+}
+
+class _EmailSignInFormState extends State<EmailSignInForm> {
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
 
   void _submit(){
-    print("submit: $email");
+    print("email: ${_emailController.text}, password: ${_passwordController.text}");
   }
-
 
   List<Widget> _buildChildren() {
     return [
       TextField(
+        controller: _emailController,
         decoration: InputDecoration(
           labelText: "Email",
           hintText: "Test@Test.com",
         ),
-        onChanged: (value) => email = value,
       ),
       SizedBox(height: 8),
       TextField(
+        controller: _passwordController,
         obscureText: true,
         decoration: InputDecoration(
           labelText: "Password",
