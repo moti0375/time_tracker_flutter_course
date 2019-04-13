20199329baa338786e41796185b5bf4da03875de
Moti Bartov <motib@tikalk.com>
2019-04-13 15:19:46 +0300

    Auth with email and password ready

--- lib/pages/email_sign_in/email_sign_in_form.dart
+++ lib/pages/email_sign_in/email_sign_in_form.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:time_tracker_flutter_course/common_widgets/form_submit_button.dart';
+import 'package:time_tracker_flutter_course/services/auth.dart';
 
 enum EmailSignInFromType { signIn, register }
 
 class EmailSignInForm extends StatefulWidget {
+  final BaseAuth auth;
+
+  EmailSignInForm({@required this.auth});
+
   @override
   _EmailSignInFormState createState() => _EmailSignInFormState();
 }
@@ -12,16 +17,32 @@ class _EmailSignInFormState extends State<EmailSignInForm> {
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
 
+  String get _email => _emailController.text;
+
+  String get _password => _passwordController.text;
   EmailSignInFromType _formType = EmailSignInFromType.signIn;
 
-  void _submit() {
-    print(
-        "email: ${_emailController.text}, password: ${_passwordController.text}");
+  void _submit() async {
+    print("email: ${_emailController.text}, "
+        "password: ${_passwordController.text}");
+
+    try {
+      if (_formType == EmailSignInFromType.signIn) {
+        await widget.auth.signInWithEmailAndPassword(_email, _password);
+      } else {
+        await widget.auth.createAccount(_email, _password);
+      }
+      Navigator.of(context).pop();
+    } catch (e) {
+      print("There was an error: ${e.toString()}");
+    }
   }
 
   void _toggleFormType() {
     setState(() {
-      _formType = _formType == EmailSignInFromType.signIn ? EmailSignInFromType.register : EmailSignInFromType.signIn;
+      _formType = _formType == EmailSignInFromType.signIn
+          ? EmailSignInFromType.register
+          : EmailSignInFromType.signIn;
       _emailController.clear();
       _passwordController.clear();
     });
--- lib/pages/email_sign_in/email_sign_in_page.dart
+++ lib/pages/email_sign_in/email_sign_in_page.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:time_tracker_flutter_course/pages/email_sign_in/email_sign_in_form.dart';
+import 'package:time_tracker_flutter_course/services/auth.dart';
 
 class EmailSignInPage extends StatelessWidget {
+  EmailSignInPage({@required this.auth});
+
+  final BaseAuth auth;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -12,7 +17,9 @@ class EmailSignInPage extends StatelessWidget {
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Card(
-          child: EmailSignInForm(),
+          child: EmailSignInForm(
+            auth: auth,
+          ),
         ),
       ),
       backgroundColor: Colors.grey[200],
