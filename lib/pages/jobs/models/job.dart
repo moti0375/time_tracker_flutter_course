7870643fe71e82e61e89832ccc4df2eb48852fdf
Moti Bartov <motib@tikalk.com>
2019-07-27 08:26:09 +0300

    Job factory constructor

--- lib/pages/jobs/models/job.dart
+++ lib/pages/jobs/models/job.dart
@@ -4,12 +4,18 @@ class Job{
   final int ratePerHour;
 
   Job({@required this.name, @required this.ratePerHour});
+  factory Job.fromMap(Map<String, dynamic> map){
+    if(map == null){
+      return null;
+    }
+
+    String name = map['name'];
+    int ratePerHour = map['ratePerHour'];
+
+    return Job(name: name, ratePerHour: ratePerHour);
+  }
 
   Map<String, dynamic> toMap(){
     return {'name': name, 'ratePerHour' : ratePerHour};
-  }
-
-  static Job fromMap(Map<String, dynamic> map){
-    return Job(name: map["name"], ratePerHour: map["ratePerHour"]);
   }
 }
