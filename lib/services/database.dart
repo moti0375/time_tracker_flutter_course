a60dc72b96dc70e2773c3b95fbbb69f4c5b0393e
Moti Bartov <motib@tikalk.com>
2019-07-27 08:43:27 +0300

    FirestoreService singleton

--- lib/services/database.dart
+++ lib/services/database.dart
@@ -1,7 +1,7 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:meta/meta.dart';
 import 'package:time_tracker_flutter_course/pages/jobs/models/job.dart';
 import 'package:time_tracker_flutter_course/services/api_path.dart';
+import 'package:time_tracker_flutter_course/services/firestore_service.dart';
 
 abstract class Database {
   Future<void> createJob(Job job);
@@ -11,31 +11,11 @@ abstract class Database {
 
 class FirestoreDatabase implements Database {
   final String uid;
+  final FirestoreService _service = FirestoreService.instance;
 
   FirestoreDatabase({@required this.uid}) : assert(uid != null);
 
-  Future<void> createJob(Job job) async =>
-      await _setData(ApiPath.job(uid, 'job_abc'), job.toMap());
+  Future<void> createJob(Job job) async => await _service.setData(ApiPath.job(uid, 'job_abc'), job.toMap());
 
-
-
-  Stream<List<Job>> jobsStream() {
-    return _collectionStream(path: ApiPath.jobs(uid), builder: (data) => Job.fromMap(data));
-  }
-
-
-  Stream<List<T>> _collectionStream<T>({@required String path,
-  @required T builder(Map<String, dynamic> data)}){
-    final documentReference = Firestore.instance.collection(path);
-    final snapshots = documentReference.snapshots();
-    return snapshots.map(
-          (snapshot) => snapshot.documents.map((doc) => builder(doc.data)).toList()
-    );
-  }
-
-  Future<void> _setData(String path, Map<String, dynamic> data) async {
-    final documentReference = Firestore.instance.document(path);
-    print("FirestoreDatabase: _setData: $path: $data");
-    await documentReference.setData(data);
-  }
+  Stream<List<Job>> jobsStream() =>  _service.collectionStream(path: ApiPath.jobs(uid), builder: (data) => Job.fromMap(data));
 }
---
+++ lib/services/firestore_service.dart
@@ -0,0 +1,23 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:meta/meta.dart';
+
+class FirestoreService{
+
+  FirestoreService._();
+  static final instance = FirestoreService._();
+
+  Stream<List<T>> collectionStream<T>({@required String path,
+    @required T builder(Map<String, dynamic> data)}){
+    final documentReference = Firestore.instance.collection(path);
+    final snapshots = documentReference.snapshots();
+    return snapshots.map(
+            (snapshot) => snapshot.documents.map((doc) => builder(doc.data)).toList()
+    );
+  }
+
+  Future<void> setData(String path, Map<String, dynamic> data) async {
+    final documentReference = Firestore.instance.document(path);
+    print("FirestoreDatabase: _setData: $path: $data");
+    await documentReference.setData(data);
+  }
+}
