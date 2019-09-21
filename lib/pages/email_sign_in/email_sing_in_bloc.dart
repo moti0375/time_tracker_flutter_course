1d806ab058ec4805853fc066871a9760100438ad
Moti Bartov <motib@tikalk.com>
2019-09-21 16:16:47 +0300

    Entries page file import, add RxDart

--- lib/pages/email_sign_in/email_sing_in_bloc.dart
+++ lib/pages/email_sign_in/email_sing_in_bloc.dart
@@ -3,16 +3,18 @@ import 'dart:async';
 import 'package:meta/meta.dart';
 import 'package:time_tracker_flutter_course/pages/email_sign_in/email_sign_in_model.dart';
 import 'package:time_tracker_flutter_course/services/auth.dart';
+import 'package:rxdart/rxdart.dart';
 
 class EmailSignInBloc {
   EmailSignInBloc({@required this.auth});
 
-  final StreamController<EmailSignInModel> _modelController =
-      StreamController<EmailSignInModel>();
+  final _modelSubject =
+      BehaviorSubject<EmailSignInModel>.seeded(EmailSignInModel());
   final BaseAuth auth;
 
-  Stream<EmailSignInModel> get modelStream => _modelController.stream;
-  EmailSignInModel _currentModel = EmailSignInModel();
+  Observable<EmailSignInModel> get modelStream => _modelSubject.stream;
+
+  EmailSignInModel get _currentModel => _modelSubject.value;
 
   void toggleFormType() {
     final type = _currentModel.formType == EmailSignInFormType.signIn
@@ -29,6 +31,7 @@ class EmailSignInBloc {
   }
 
   void updateEmail(String email) => updateWith(email: email.trim());
+
   void updatePassword(String password) => updateWith(password: password.trim());
 
   void updateWith({
@@ -38,15 +41,14 @@ class EmailSignInBloc {
     bool loading,
     bool submitted,
   }) {
-    _currentModel = _currentModel.copy(
+    //Update model and add updated model to controller
+    _modelSubject.value = _currentModel.copy(
       email,
       password,
       formType,
       loading,
       submitted,
     );
-    //Update model and add updated model to controller
-    _modelController.add(_currentModel);
   }
 
   Future<void> submit() async {
@@ -57,7 +59,7 @@ class EmailSignInBloc {
         "password: ${_currentModel.password.trim()}");
 
     try {
-    //  await Future.delayed(Duration(seconds: 5)); //Simulating a slow network
+      //  await Future.delayed(Duration(seconds: 5)); //Simulating a slow network
       if (_currentModel.formType == EmailSignInFormType.signIn) {
         await auth.signInWithEmailAndPassword(
             _currentModel.email, _currentModel.password);
@@ -73,6 +75,6 @@ class EmailSignInBloc {
   }
 
   void dispose() {
-    _modelController.close();
+    _modelSubject.close();
   }
 }
