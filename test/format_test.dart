673051b3a8c2a6e86dbcb76d0641c86599a9daab
Moti Bartov <motib@tikalk.com>
2019-09-28 18:56:34 +0300

    Currency Format tests

--- test/format_test.dart
+++ test/format_test.dart
@@ -35,4 +35,60 @@ void main(){
       expect(Format.date(DateTime(2019, 9, 16)), '16 Aug 2019');
     });
   });
+  
+  group("Day of week - IT Local", (){
+    setUp(() async {
+      Intl.defaultLocale = 'it_IT';
+      await initializeDateFormatting(Intl.defaultLocale);
+    });
+
+    test("Day of week today", (){
+      expect(Format.dayOfWeek(DateTime.now()), 'sab');
+    });
+
+
+    test("Day of week Sunday", (){
+      expect(Format.dayOfWeek(DateTime(2019, 9, 29)), 'dom');
+    });
+
+  });
+
+  group("Day of week - HE Local", (){
+    setUp(() async {
+      Intl.defaultLocale = 'he_HE';
+      await initializeDateFormatting(Intl.defaultLocale);
+    });
+
+    test("Day of week today", (){
+      expect(Format.dayOfWeek(DateTime.now()), 'שבת');
+    });
+
+
+    test("Day of week Sunday", (){
+      expect(Format.dayOfWeek(DateTime(2019, 9, 29)), 'יום א׳');
+    });
+
+  });
+
+
+  group("Currency US Local", (){
+    setUp(() async {
+      Intl.defaultLocale = 'en_US';
+      await initializeDateFormatting(Intl.defaultLocale);
+    });
+
+    test("100\$", (){
+      expect(Format.currency(100.0), '\$100');
+    });
+
+
+    test("1000\$", (){
+      expect(Format.currency(1000.0), '\$1,000');
+    });
+
+    test("0 currency", (){
+      expect(Format.currency(0), '');
+    });
+
+  });
 }
