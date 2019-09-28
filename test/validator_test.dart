e792f5e59811b2acac478bfbd02918a5a8fdbf0d
Moti Bartov <motib@tikalk.com>
2019-09-28 15:12:42 +0300

    Separating test

--- test/validator_test.dart
+++ test/validator_test.dart
@@ -5,7 +5,10 @@ void main(){
   test("None Empty String Test", (){
     final validator = NonEmptyStringValidator();
     expect(validator.isValid('test'), true);
+  });
 
-    expect(validator.isValid(""), false);
+  test("Empty String Test", (){
+    final validator = NonEmptyStringValidator();
+    expect(validator.isValid(''), false);
   });
 }
